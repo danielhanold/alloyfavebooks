f550b069c16fd90a939333a9e932557e83180c04
Daniel Hanold <danny@danielhanold.com>
2014-02-10 10:58:01 -0500

    Focus title text field when user opens addbook window

--- app/controllers/addbook.js
+++ app/controllers/addbook.js
@@ -3,6 +3,12 @@ var args = arguments[0] || {};
 // Access the existing books collection.
 var myBooks = Alloy.Collections.books;
 
+// When window opens, focus the title field.
+$.addbook.addEventListener('open', function(e) {
+  $.titleInput.focus();
+  Ti.API.error('window got opened. focus text field');
+});
+
 function addBook(e) {
   Ti.API.error(e);
 };
